7595c4dcb385a03b5323a50e74724fd2790267a9
VachanGupta <myselfvachangupta@gmail.com>
2025-07-28 09:59:09 +0530

    changes to rows and columns

--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -4,7 +4,7 @@ import {dijkstra, getNodesInShortestPathOrder} from '../algorithms/dijkstra';
 
 import './PathfindingVisualizer.css';
 
-const START_NODE_ROW = 10;
+const START_NODE_ROW = 15;
 const START_NODE_COL = 15;
 const FINISH_NODE_ROW = 10;
 const FINISH_NODE_COL = 35;
@@ -79,7 +79,7 @@ export default class PathfindingVisualizer extends Component {
     return (
       <>
         <button onClick={() => this.visualizeDijkstra()}>
-          Visualize Dijkstra's Algorithm
+          Visualize the algorithm
         </button>
         <div className="grid">
           {grid.map((row, rowIdx) => {
